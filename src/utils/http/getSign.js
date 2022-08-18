47621dd46a09b0162c1089f1accf4b31897ae596
Joly <cql.ql@qq.com>
2022-08-18 09:44:37 +0800

    优化 getSign

--- src/utils/http/getSign.js
+++ src/utils/http/getSign.js
@@ -1,49 +1,36 @@
 import md5 from 'crypto-js/md5'
 
 //获取签名
-
-export default function getSign(obj, token) {
+export default function getSign(data, token) {
   // const token = ''
-  // 递归排序
-  function sort(obj) {
-    if (obj instanceof Array) {
-      //如果数组里面存放的为对象,通过map更改数组结构，排序
-      obj = obj.map((ele) => {
-        if (ele instanceof Object) {
-          const newObj = {}
-          Object.keys(ele)
-            .sort()
-            .forEach(function (key) {
-              let o = ele[key]
-              if (o instanceof Object) {
-                o = sort(o)
-              }
-              newObj[key] = o
-            })
-          ele = newObj
-        }
-        return ele
-      })
-      return obj
-    }
 
+  function dataSort(obj) {
+    if (obj instanceof Array) {
+      return obj.map((currObj) => {
+        return dataSort(currObj)
+      })
+    }
+    if (obj instanceof Object) {
+      return objSort(obj)
+    }
+    return obj
+  }
+
+  function objSort(obj) {
     const newObj = {}
-    //默认情况下，对字符串排序，是按照ASCII的大小比较的，排序忽略大小写按照字母序排序。
     Object.keys(obj)
-      .sort()
+      .sort((a, b) => a.localeCompare(b))
       .forEach(function (key) {
-        let o = obj[key]
-        if (o instanceof Object) {
-          o = sort(o)
-        }
-        newObj[key] = o
+        newObj[key] = dataSort(obj[key])
       })
     return newObj
   }
 
+  let sortedData = dataSort(data)
+
   let dataStr = ''
-  if (obj !== undefined) {
-    dataStr = JSON.stringify(sort(obj))
+  if (sortedData !== undefined) {
+    dataStr = JSON.stringify(sortedData)
   }
 
   return md5(dataStr + ')(4AzEdr5J6a`@#$*%' + token).toString()
