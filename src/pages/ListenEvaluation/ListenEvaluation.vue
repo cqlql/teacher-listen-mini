338333cb498167e7c3aaf070318d2aa339e9051b
cqlql <cql.ql@qq.com>
2022-08-31 16:22:53 +0800

    Update ListenEvaluation.vue

--- src/pages/ListenEvaluation/ListenEvaluation.vue
+++ src/pages/ListenEvaluation/ListenEvaluation.vue
@@ -1,17 +1,9 @@
 <script lang="ts" setup>
 import ToastProvider from '@/components/ToastProvider/ToastProvider.vue'
-import { ref } from 'vue'
 import ListenEvaluationMain from './ListenEvaluationMain.vue'
-const v = ref('1111')
-
-function onClick() {
-  v.value = 'xxxxxxxxxxx'
-}
-const vVal = '22222'
 </script>
 <template>
-  {{ v }}{{ vVal }}
-  <ToastProvider @click="onClick">
+  <ToastProvider>
     <ListenEvaluationMain> </ListenEvaluationMain>
   </ToastProvider>
 </template>
